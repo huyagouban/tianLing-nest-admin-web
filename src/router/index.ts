4dd7700b14bd4fea8049323d58f68c7805686272
陆天凌 <1096428305@qq.com>
2024-08-11 07:36:17 +0800

    修改配置文件

--- src/router/index.ts
+++ src/router/index.ts
@@ -93,7 +93,7 @@ export const filterDynamicsRouters = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.NODE_ENV==='production'?'/tianLing-nest-admin-web/': import.meta.env.BASE_URL),
   routes: constanRouters
 })
 
